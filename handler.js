0f0d4ab0a18c09379c8b6ed459718d0da5615e41
Nichola Riley <nicholajaneriley@gmail.com>
2020-01-29 19:57:58 +0000

    PUT working as expected with SQL DB on AWS

--- handler.js
+++ handler.js
@@ -4,7 +4,7 @@ const serverlessHttp = require("serverless-http");
 const bodyParser = require("body-parser");
 const mysql = require("mysql");
 
-const app = express ();
+const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
@@ -34,27 +34,26 @@ app.get("/todotada", function (request, response) {
 
 
 // POST
-app.post("/todotada", function(request, response) {
+app.post("/todotada", function (request, response) {
   const newTask = request.body;
-  connection.query("INSERT INTO Task SET ?", [newTask], function (err, data){
+  connection.query("INSERT INTO Task SET ?", [newTask], function (err, data) {
     if (err) {
       response.status(500).json({
         error: err
       });
     } else {
-     newTask.id = data.insertId; 
-     response.status(201).json(newTask);
+      newTask.id = data.insertId;
+      response.status(201).json(newTask);
     }
   });
 });
- 
-// PUT
 
-app.put("/todotada/:id", function(request, response) {
+//PUT
+app.put("/todotada/:id", function (request, response) {
   const updatedTask = request.body;
   const id = request.params.id;
 
-  connection.query(`UPDATE Task SET ? WHERE TaskID=?`[updatedTask, id],
+  connection.query(`UPDATE Task SET ? WHERE TaskID=?`, [updatedTask, id],
     function (err) {
       if (err) {
         response.status(500).json({ error: err });
@@ -65,21 +64,21 @@ app.put("/todotada/:id", function(request, response) {
   );
 });
 
-// DELETE
 
-app.delete("/todotada/:id", function(request, response) {
-    const id = request.params.id;
-    connection.query("DELETE FROM Task WHERE TaskID=?", [id], function (err){
-      if (err) {
-        response.status(500).json({
-          error: err
-        });
-      } else {
-        response.sendStatus(200);
-      }
-    });
+// DELETE
+app.delete("/todotada/:id", function (request, response) {
+  const id = request.params.id;
+  connection.query("DELETE FROM Task WHERE TaskID=?", [id], function (err) {
+    if (err) {
+      response.status(500).json({
+        error: err
+      });
+    } else {
+      response.sendStatus(200);
+    }
   });
- 
+});
+
 
 
 module.exports.app = serverlessHttp(app);
